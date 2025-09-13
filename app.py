c228dce3208121273ef36d1b9b8c2a074a1c4437
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-13 14:04:03 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -417,11 +417,11 @@ def main():
                 flex-grow: 1;
                 height: 400px;
                 overflow-y: scroll;
-                /* -ms-overflow-style: none;
+                /* -ms-overflow-style: none;*/
                 scrollbar-width: auto;
             }
             .gift-list-container::-webkit-scrollbar {
-                /* display: none;
+                /* display: none;*/
             }
             .gift-item {
                 display: flex;
