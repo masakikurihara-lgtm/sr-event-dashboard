d5ee637f0fb606560ea87c59ab1ee02c784b6792
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-13 19:53:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -544,7 +544,7 @@ def main():
 
                                 highlight_class = ""
                                 if total_point >= 100:
-                                    highlight_class = "highlight-30000"
+                                    highlight_class = "highlight-60000"
 #                                if gift_point >= 500:
 #                                    if total_point >= 300000:
 #                                        highlight_class = "highlight-300000"
