3c228609cf7b19f464e4df32bf48fb95255aeeb4
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-13 19:37:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -536,7 +536,7 @@ def main():
                             for log in gift_log:
                                 gift_id = log.get('gift_id')
                                 # ★ 修正箇所: get_gift_listでキーを文字列に変換したため、ここでも文字列キーで検索する
-                                gift_info = gift_list_map.get(str(gift_id), {})
+                                gift_info = gift_list_map.get(int(gift_id), {})
                                 
                                 gift_point = gift_info.get('point', 0)
                                 gift_count = log.get('num', 0)
