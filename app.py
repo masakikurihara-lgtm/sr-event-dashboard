4ab5c3ed1c1c93425cdbcf92e08e70ffd376eda8
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-17 02:45:38 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -350,7 +350,7 @@ def main():
                 return
 
             st.markdown("<h2 style='font-size:2em;'>3. リアルタイムダッシュボード</h2>", unsafe_allow_html=True)
-            st.info("10秒ごとに自動更新されます。")
+            st.info("7秒ごとに自動更新されます。")
 
             #st.markdown("<div style='margin-top: 0px;'></div>", unsafe_allow_html=True)
 
