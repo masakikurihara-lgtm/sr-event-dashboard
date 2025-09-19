5007f40b88e9b61676288c0cbfd5848afea31e23
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-19 23:11:09 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -625,7 +625,7 @@ def main():
                 gift_history_title += " <span style='font-size: 14px;'>（現在配信中のルームのみ表示）</span>"
             st.markdown(f"### {gift_history_title}", unsafe_allow_html=True)
 
-            #st.markdown("<div style='margin-bottom: 16px;'></div>", unsafe_allow_html=True)
+            st.markdown("<div style='margin-bottom: 16px;'></div>", unsafe_allow_html=True)
             gift_container = st.container()
             
             css_style = """
