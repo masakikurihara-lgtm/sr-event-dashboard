07d54f21461370be7afb8a8161b50127d346163e
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-19 19:10:06 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -852,7 +852,7 @@ def main():
                     else:
                         st.info(f"対象 `{selected_target_room}` と `{selected_enemy_room}` のポイントは同点です（{target_point:,} pt）。")
 
-                    st.markdown(f"- 対象の現在順位: **{target_rank if target_rank is not None else 'N/A'}**")
+                    st.markdown(f"- 対象の現在順位: **{target_rank if target_rank is not None else 'N/A'}位**")
                     #st.markdown(f"- 対象の現在ポイント: **{target_point:,} pt**")
                     #st.markdown(f"- 対象の下位とのポイント差: **{target_lower_gap if target_lower_gap is not None else 'N/A'}**")
 
