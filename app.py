49ae00a4ac1a8ef9f108670fef4220fb11415a99
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-09-27 13:05:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -947,9 +947,9 @@ def main():
 
             is_block_event = selected_event_data.get("is_event_block", False)
             block_event_ranks = {}
-            #if is_block_event and not is_event_ended:
-            #    with st.spinner('ブロックイベントの全体順位を取得中...'):
-            #        block_event_ranks = get_block_event_overall_ranking(selected_event_data.get('event_url_key'))
+            if is_block_event and not is_event_ended:
+                with st.spinner('ブロックイベントの全体順位を取得中...'):
+                    block_event_ranks = get_block_event_overall_ranking(selected_event_data.get('is_event_block'))
 
             if st.session_state.selected_room_names:
                 premium_live_rooms = [
