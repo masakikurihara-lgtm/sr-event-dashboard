b8190147fc70b24bdc5bd6ed10add554775868fc
masakikurihara-lgtm <masaki.kurihara@gmail.com>
2025-10-19 01:39:14 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -881,13 +881,63 @@ def main():
                         col1, col2 = st.columns([1, 1])
                         with col1:
                             st.components.v1.html(f"""
-                            <div style="font-weight: bold; font-size: 1.5rem; color: #333333; line-height: 1.2; padding-bottom: 15px;">イベント期間</div>
-                            <div style="font-weight: bold; font-size: 1.1rem; color: #333333; line-height: 1.2;">{event_period_str}</div>
+                            <style>
+                              .event-period-text {{
+                                font-weight: bold;
+                                font-size: 1.5rem;
+                                color: #333333;
+                                line-height: 1.2;
+                                padding-bottom: 15px;
+                              }}
+                              .event-period-text2 {{
+                                font-weight: bold;
+                                font-size: 1.1rem;
+                                color: #333333;
+                                line-height: 1.2;
+                              }}
+                            @media screen and (max-width: 767px) {{
+                              .event-period-text {{
+                                white-space: normal !important;
+                                word-break: break-word !important;
+                                overflow: visible !important;
+                                height: auto !important;
+                                display: block !important;
+                                font-weight: bold;
+                                font-size: 1.2rem !important;
+                                color: #333333;
+                                line-height: 1.2;
+                                padding-bottom: 15px;
+                              }}
+                              .event-period-text2 {{
+                                white-space: normal !important;
+                                word-break: break-word !important;
+                                overflow: visible !important;
+                                height: auto !important;
+                                display: block !important;
+                                font-weight: bold;
+                                font-size: 0.8rem !important;
+                                color: #333333;
+                                line-height: 1.2;
+                              }}
+                              .event-period-text3 {{
+                                white-space: normal !important;
+                                word-break: break-word !important;
+                                overflow: visible !important;
+                                height: auto !important;
+                                display: block !important;
+                                font-weight: bold;
+                                font-size: 0.8rem !important;
+                                line-height: 1.2;
+                              }}
+                            }}
+                            </style>
+                            <div class="event-period-text">イベント期間</div>
+                            <div class="event-period-text2">{event_period_str}</div>
                             """, height=80)
                         with col2:
                             st.components.v1.html(f"""
-                            <div style="font-weight: bold; font-size: 1.5rem; color: #333333; line-height: 1.2; padding-bottom: 15px;">残り時間</div>
-                            <div style="font-weight: bold; font-size: 1.1rem; line-height: 1.2;">
+                            <div class="event-period-text">残り時間</div>
+                            <div class="event-period-text3">
                                 <span id="sr_countdown_timer_in_col" style="color: #4CAF50;" data-end="{int(ended_at_dt.timestamp() * 1000)}">計算中...</span>
                             </div>
                             </div>
